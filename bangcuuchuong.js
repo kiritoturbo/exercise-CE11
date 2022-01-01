f99f344e2c2868dc69aeb067de709018e9f031c2
kirito <manhtruong@gmail.com>
2022-01-01 09:26:40 +0700

    bai cuu chuong băng javascript

---
+++ bangcuuchuong.js
@@ -0,0 +1,11 @@
+var c=prompt("nhập số:");
+for(var x=0;x<=10;x++){
+    // console.log(x+"*"+c+"="+x*c);
+    document.write(x+"*"+c+"="+x*c+"\n");
+    document.write("<br>");
+}
+
+// in ra số chẵn 
+// for(var i =0;i<11;i=i+2){
+//     console.log(i);
+// }
